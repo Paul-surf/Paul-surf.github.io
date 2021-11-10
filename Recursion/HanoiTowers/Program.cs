2a4e30157c48449f8fd255c6cb7217d658a925e2
Paul-surf <fanprodkaqw@hotmail.com>
2021-11-11 00:04:03 +0100

    hanoi towers

---
+++ Recursion/HanoiTowers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HanoiTowers
+{
+    class Program
+    {
+        /*
+        n: number of disks
+        f: 'from' position
+        h: 'helper' position
+        t: 'target' position
+        */
+        static void Main(string[] args)
+        { 
+            //           n  f  t  h
+            TowerofHanoi(3, 1, 3, 2);
+        }
+        public static void TowerofHanoi(int n, int f, int t, int h)
+        {
+            if (n == 1)
+            {
+                System.Console.WriteLine("Move disk from pole " + f + " to pole " + t);
+            }
+            else
+            {
+                TowerofHanoi(n - 1, f, h, t);
+                TowerofHanoi(1, f, t, h);
+                TowerofHanoi(n - 1, h, t, f);
+            }
+        }
+    }
+}
---
+++ ToReadList.md
@@ -0,0 +1,1 @@
+navigation list styling
